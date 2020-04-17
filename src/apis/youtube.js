77a536dc14e16bcb965d0a3c2cd5356b4cd04189
Jaime <jaimesof82@gmail.com>
2020-04-16 21:57:07 -0700

    Added nte for axios bug with params object

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//Had to move the params object to app.js because of axios bug w/ v0.19
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3"
 });
